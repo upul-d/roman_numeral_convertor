d2885cc7c0a260d3a330af63a031ba1cdb771f54
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-10-08 02:26:10 +0100

    Adds StringBuilder

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -1,11 +1,11 @@
 public class RomanNumeral implements RomanNumeralGenerator {
     public String generate(int number) {
-        String romanNumeral = "";
+        StringBuilder romanNumeral = new StringBuilder();
 
         if (number < 1 || number > 3999) {
             throw new IllegalArgumentException("Sorry, please enter a number between 1 and 3999 inclusive");
         }
 
-        return romanNumeral;
+        return romanNumeral.toString();
     }
 }

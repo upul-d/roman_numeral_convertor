99a769607647e5309194aa0bfa6bd424ad77e5ac
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-10-08 05:08:05 +0100

    Refactors to abstract out logic from generate method

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -1,4 +1,19 @@
 public class RomanNumeral implements RomanNumeralGenerator {
+    private void getThousands(int number, StringBuilder romanNumeral) {
+        // Handle thousands logic
+        getHundreds(number, romanNumeral);
+    }
+
+    private void getHundreds(int number, StringBuilder romanNumeral) {
+        // Handle hundreds logic
+        getTens(number, romanNumeral);
+    }
+
+    private void getTens(int number, StringBuilder romanNumeral) {
+        // Handle tens logic
+        getOnes(number, romanNumeral);
+    }
+
     private void getOnes(int number, StringBuilder romanNumeral) {
         if (number == 9) {
             romanNumeral.append("IX");
@@ -17,14 +32,16 @@ public class RomanNumeral implements RomanNumeralGenerator {
         }
     }
 
-    public String generate(int number) {
+    private String getUnits(int number) {
         StringBuilder romanNumeral = new StringBuilder();
+        getThousands(number, romanNumeral);
+        return romanNumeral.toString();
+    }
 
+    public String generate(int number) {
         if (number < 1 || number > 3999) {
             throw new IllegalArgumentException("Sorry, please enter a number between 1 and 3999 inclusive");
         }
-
-        getOnes(number, romanNumeral);
-        return romanNumeral.toString();
+        return getUnits(number);
     }
 }

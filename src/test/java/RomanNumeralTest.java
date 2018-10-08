5f20c31da642f1e0247b53857cadb528c0df36e1
Upul Dissanayaka <u.dissanayaka@gmail.com>
2018-10-08 01:21:22 +0100

    Passes test for handling input below lower limit

---
+++ src/test/java/RomanNumeralTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class RomanNumeralTest {
+    private RomanNumeral romanNumeral;
+
+    @Before
+    public void before() {
+        romanNumeral = new RomanNumeral();
+    }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void numberBelowLowerLimitThrowsException() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Sorry, please enter a number between 1 and 3999 inclusive");
+        romanNumeral.generate(0);
+    }
+
+}
+
